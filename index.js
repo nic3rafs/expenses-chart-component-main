d081b813c6fea79526846f17f6469fa444e5ade0
partachu.eth <43925652+partachu@users.noreply.github.com>
2023-02-08 20:07:12 +0100

    changed import json to fetch

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-import data from "./data.json" assert { type: "json" };
+const resp = await fetch('data.json')
+const data = await resp.json()
 
 const chart = document.querySelector(".chart");
-const maxAmount = Math.max(...data.map(object => object.amount))
+const maxAmount = Math.max(...data.map((object) => object.amount));
 
 for (const value of data) {
   const bar = document.createElement("div");
   bar.classList.add("bar");
- 
+
   //first div, amount
-  const firstDiv = document.createElement("div")
-  firstDiv.innerHTML = `$${value.amount}`
-  firstDiv.setAttribute('class', 'amount')
+  const firstDiv = document.createElement("div");
+  firstDiv.innerHTML = `$${value.amount}`;
+  firstDiv.setAttribute("class", "amount");
 
   //second div, bar
-  const secondDiv = document.createElement("div")
-  secondDiv.setAttribute('class', 'bar-column')
-  const height = ((value.amount / maxAmount) * 120) + 'px' // calculate bar height (max height 120px)
-  secondDiv.style.height = height
-  secondDiv.setAttribute('data-amount', value.amount)
-  secondDiv.setAttribute('id',`${value.day}`)
+  const secondDiv = document.createElement("div");
+  secondDiv.setAttribute("class", "bar-column");
+  const height = (value.amount / maxAmount) * 120 + "px"; // calculate bar height (max height 120px)
+  secondDiv.style.height = height;
+  secondDiv.setAttribute("data-amount", value.amount);
+  secondDiv.setAttribute("id", `${value.day}`);
   if (getDayName() === value.day) {
-    secondDiv.classList.add('today-bar')
+    secondDiv.classList.add("today-bar");
   }
 
   // day
-  const p = document.createElement("p")
-  p.innerHTML = `${value.day}`
+  const p = document.createElement("p");
+  p.innerHTML = `${value.day}`;
 
-  bar.appendChild(firstDiv)
-  bar.appendChild(secondDiv)
-  bar.appendChild(p)
-  chart.appendChild(bar)
+  bar.appendChild(firstDiv);
+  bar.appendChild(secondDiv);
+  bar.appendChild(p);
+  chart.appendChild(bar);
 }
 
-function getDayName(date = new Date(), locale = 'en-US') {
-  return date.toLocaleDateString(locale, {weekday: 'long'}).substring(0,3).toLowerCase()
+function getDayName(date = new Date(), locale = "en-US") {
+  return date
+    .toLocaleDateString(locale, { weekday: "long" })
+    .substring(0, 3)
+    .toLowerCase();
 }
